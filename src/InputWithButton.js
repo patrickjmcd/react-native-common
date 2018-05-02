32006f8b6f9e6af3552989cd4dfe432cae0b34c0
Patrick McDonagh <patrickjmcd@gmail.com>
2018-05-02 18:29:32 -0500

    button was missing from InputWithButton

--- src/InputWithButton.js
+++ src/InputWithButton.js
@@ -1,9 +1,10 @@
 import React from 'react';
-import { TextInput, View, Text } from 'react-native';
+import { TextInput, View, Text, TouchableOpacity } from 'react-native';
 
 const InputWithButton = ({ label, onChangeText, value, placeholder, 
-    autoCorrect, autoCapitalize, secureTextEntry, keyboardType }) => {
-    const { labelStyle, inputStyle, containerStyle } = styles;
+    autoCorrect, autoCapitalize, secureTextEntry, keyboardType, 
+    onButtonPress, buttonText }) => {
+    const { labelStyle, inputStyle, containerStyle, buttonStyle, buttonTextStyle } = styles;
     return (
         <View style={containerStyle}>
             <Text style={labelStyle}>{label}</Text>
@@ -17,6 +18,11 @@ const InputWithButton = ({ label, onChangeText, value, placeholder,
                 value={value}
                 onChangeText={onChangeText}
             />
+            <TouchableOpacity onPress={onButtonPress} style={buttonStyle}>
+                <Text style={buttonTextStyle}>
+                    {buttonText}
+                </Text>
+            </TouchableOpacity>
         </View>
     );
 };
@@ -25,7 +31,7 @@ const styles = {
     labelStyle: {
         fontSize: 18,
         paddingLeft: 20,
-        flex: 1
+        flex: 3
     },
     inputStyle: {
         color: '#000',
@@ -40,6 +46,26 @@ const styles = {
         flex: 1,
         flexDirection: 'row',
         alignItems: 'center'
+    },
+
+    buttonTextStyle: {
+        alignSelf: 'center',
+        color: '#007aff',
+        fontSize: 16,
+        fontWeight: '600',
+        paddingTop: 10,
+        paddingBottom: 10
+    },
+
+    buttonStyle: {
+        flex: 1,
+        alignSelf: 'stretch',
+        backgroundColor: '#fff',
+        borderRadius: 5,
+        borderWidth: 1,
+        borderColor: '#007aff',
+        marginLeft: 5,
+        marginRight: 5
     }
 };
 
